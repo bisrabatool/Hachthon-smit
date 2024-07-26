f55f840d93583f555da40648a1e08e536ac486e1
DuresameenShafi <duresameen32@gmail.com>
2024-07-26 01:51:28 -0700

    home page Setup

--- quiz-app/frontend/src/App.js
+++ quiz-app/frontend/src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
-import Dashboard from "./components/dashboard";
+import Dashboard from "./components/pages/dashboard";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Register from "./components/register";
-import Login from "./components/login";
+import Register from "./components/pages/register";
+import Login from "./components/pages/login";
+import Home from "./components/pages/home"
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+      <Route path="/home" element={<Home/>} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
